9bce9a3b616d94d7fb93bd6bb976bc3916c2f274
Maxim Kholod <maxim.kholod@elastic.co>
2026-04-16 15:14:37 +0200

    Update copyright headers to reference Elastic License 2.0 explicitly
    Use the canonical elastic-license-2.0-header.txt from elastic/open-source
    to match Kibana's current practice and avoid ambiguity with ELv1.

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
--- start-mcp.sh
+++ start-mcp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-# or more contributor license agreements. Licensed under the Elastic License;
-# you may not use this file except in compliance with the Elastic License.
+# or more contributor license agreements. Licensed under the Elastic License
+# 2.0; you may not use this file except in compliance with the Elastic License
+# 2.0.
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
